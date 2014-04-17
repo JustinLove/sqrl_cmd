b8e54f720376e1ebc8e2768c480b1510281b5e27
Justin Love <git@JustinLove.name>
2014-04-17 16:18:46 -0500

    primitive post command

--- lib/sqrl/cmd.rb
+++ lib/sqrl/cmd.rb
@@ -1,6 +1,7 @@
 require "sqrl/cmd/version"
 require "sqrl/authentication_query"
 require "thor"
+require "httpclient"
 
 module SQRL
   class Cmd < Thor
@@ -10,6 +11,16 @@ module SQRL
       p request.client_data
       puts "POST #{request.url}\n\n"
       puts request.post_body
+
+      request
+    end
+
+    desc 'post login', 'Sign the provided url and attempt to log in'
+    def post(url)
+      request = sign(url)
+      response = HTTPClient.new.post(request.url, request.post_body)
+      puts response.body
+      puts "Response: #{response.status}"
     end
   end
 end
