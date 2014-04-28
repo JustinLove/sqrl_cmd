e1f8a25ffb9780c06cf6b80e5e3e6c99724b46fb
Justin Love <git@JustinLove.name>
2014-04-28 17:07:08 -0500

    attempt to log in

--- lib/sqrl/cmd.rb
+++ lib/sqrl/cmd.rb
@@ -24,5 +24,21 @@ module SQRL
       puts response.body
       puts "Response: #{response.status}"
     end
+
+    desc 'attempt login', 'Sign the provided url and attempt to complete login'
+    def login(url)
+      req1 = sign(url)
+      res1 = HTTPClient.new.post(req1.post_path, req1.post_body)
+      puts res1.body
+      puts "Response 1: #{res1.status}"
+
+      req2 = SQRL::AuthenticationQueryGenerator.new(req1.session, res1.body).login!
+      p req2.client_data
+      puts "POST #{req2.post_path}\n\n"
+      puts req2.post_body
+      res2 = HTTPClient.new.post(req2.post_path, req2.post_body)
+      puts res2.body
+      puts "Response 2: #{res2.status}"
+    end
   end
 end
