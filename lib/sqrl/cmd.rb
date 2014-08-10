ff14f10103efd83d2e43f67cc6b20753bdc08860
Justin Love <git@JustinLove.name>
2014-08-10 16:33:38 -0500

    single-loop login

--- lib/sqrl/cmd.rb
+++ lib/sqrl/cmd.rb
@@ -1,6 +1,7 @@
 require "sqrl/cmd/version"
 require "sqrl/client_session"
 require "sqrl/authentication_query_generator"
+require "sqrl/authentication_response_parser"
 require "thor"
 require "httpclient"
 
@@ -25,8 +26,23 @@ module SQRL
       puts "Response: #{response.status}"
     end
 
-    desc 'attempt login', 'Sign the provided url and attempt to complete login'
+    desc 'attempt one-step login', 'Sign the provided url and attempt to complete login'
     def login(url)
+      session = ClientSession.new(url, 'x'*32)
+      req1 = AuthenticationQueryGenerator.new(session, url).login!
+      p req1.client_data
+      puts "POST #{req1.post_path}\n\n"
+      puts req1.post_body
+      res1 = HTTPClient.new.post(req1.post_path, req1.post_body)
+      puts "Response 1: #{res1.status}"
+      puts res1.body
+
+      parsed = AuthenticationResponseParser.new(session, res1.body)
+      p parsed.params
+    end
+
+    desc 'attempt second-loop login', 'Sign the provided url and attempt to complete login'
+    def loopin(url)
       req1 = sign(url)
       res1 = HTTPClient.new.post(req1.post_path, req1.post_body)
       puts res1.body
