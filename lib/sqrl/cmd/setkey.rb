d0f50556551f0df5241e1860a8173c8df01dc149
Justin Love <git@JustinLove.name>
2014-08-15 20:11:24 -0500

    provide unlock request signatures

--- lib/sqrl/cmd/setkey.rb
+++ lib/sqrl/cmd/setkey.rb
@@ -1,8 +1,22 @@
+require 'sqrl/key/unlock_request_signing'
+require 'sqrl/key/server_unlock'
+
 module SQRL
   class Cmd
     desc 'setkey [URL]', 'Issue setkey command'
     def setkey(url)
-      standard_display verbose_request(url) {|req| req.setkey!}
+      session = ClientSession.new(url, imk)
+
+      res = verbose_request(url, session) {|req| req.setkey!  }
+      standard_display res
+
+      if res.command_failed? && res.suk? && identity_unlock_key
+        suk = Key::ServerUnlock.new(res.suk)
+        ursk = Key::UnlockRequestSigning.new(suk, identity_unlock_key)
+        standard_display verbose_request(url, session) {|req|
+          req.setkey!.unlock(ursk)
+        }
+      end
     end
   end
 end
--- lib/sqrl/cmd/setlock.rb
+++ lib/sqrl/cmd/setlock.rb
@@ -1,14 +1,28 @@
 require "sqrl/base64"
+require 'sqrl/key/unlock_request_signing'
+require 'sqrl/key/server_unlock'
 
 module SQRL
   class Cmd
     desc 'setlock [URL]', 'Send the server and verify unlock keys'
     def setlock(url)
-      response = verbose_request(url) {|req|
+      session = ClientSession.new(url, imk)
+
+      res = verbose_request(url, session) {|req|
         req.setlock!(identity_lock_key.unlock_pair)
       }
-      standard_display response
-      puts Base64.encode(response.suk)
+      standard_display res
+      puts Base64.encode(res.suk)
+
+      if res.command_failed? && res.suk? && identity_unlock_key
+        suk = Key::ServerUnlock.new(res.suk)
+        ursk = Key::UnlockRequestSigning.new(suk, identity_unlock_key)
+        res =  verbose_request(url, session) {|req|
+          req.setlock!(identity_lock_key.unlock_pair).unlock(ursk)
+        }
+        standard_display res
+        puts Base64.encode(res.suk)
+      end
     end
   end
 end
